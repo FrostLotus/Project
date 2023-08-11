c166c90efe7a395ada319e301ad1c65534cc66d9
friderick <wr264359@gmail.com>
2023-08-11 16:25:43 +0800

    First

---
+++ MXproject/MX_test/MX_Component/MX_Component.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Timers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ActProgTypeLib;
+using ActUtlTypeLib;
+using ActSupportMsgLib;
+
+namespace MXComponent
+{
+    
+}
