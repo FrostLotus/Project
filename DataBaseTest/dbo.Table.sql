35d477f11920394e05235eff417f1789ecbccefd
friderick <wr264359@gmail.com>
2023-08-15 17:15:50 +0800

    DataBase TEST

---
+++ DataBaseTest/dbo.Table.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(10) NOT NULL, 
+    [Address] NCHAR(10) NOT NULL, 
+    [Class] NCHAR(10) NOT NULL, 
+    [A] NCHAR(10) NOT NULL, 
+    [B] NCHAR(10) NOT NULL, 
+    [C] NCHAR(10) NOT NULL, 
+    [D] NCHAR(10) NOT NULL
+)
