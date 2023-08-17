9e2d26209b7e04681899af806f36e79896942b48
friderick <wr264359@gmail.com>
2023-08-17 18:02:38 +0800

    嘗試連結線有資料庫失敗 Data Source相關參數有錯誤

--- DataBaseTest/DataBaseTest/DataBaseTest/Form1.cs
+++ DataBaseTest/DataBaseTest/DataBaseTest/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,18 @@ namespace DataBaseTest
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string connetionString;
+            SqlConnection cnn;
+            connetionString = @"Data Source=(localdb)\MVC_TestDB;Initial Catalog=D:\MS_SQL_DB\MVC_TestDB.mdf;User ID=sa;";
+            cnn = new SqlConnection(connetionString);
+            
+            cnn.Open();
+            MessageBox.Show("Connection Open  !");
+            cnn.Close();
         }
     }
 }
