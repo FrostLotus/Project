2296d5ee6ffdd24695624daa5613fff8c0195a76
frederick <wr264359@gmail.com>
2023-08-18 16:21:15 +0800

    * 新增datagridview將資料庫資料轉映至DGV上 1.目前先拉元件  完成 2.元件顯示資料表內容 完成
    新增 DATASET 與 DATATABLE 結構 作於從屬結構  須了解使用情境

--- DataBaseTest/DataBaseTest/DataBaseTest/Form1.Designer.cs
+++ DataBaseTest/DataBaseTest/DataBaseTest/Form1.Designer.cs
@@ -29,41 +29,54 @@ namespace DataBaseTest
         /// </summary>
         private void InitializeComponent()
         {
-            this.btn_Connect = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.Btn_Connect = new System.Windows.Forms.Button();
+            this.Btn_Read = new System.Windows.Forms.Button();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.Dgv_Customers = new System.Windows.Forms.DataGridView();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.dgv_Customers = new System.Windows.Forms.DataGridView();
+            this.Dgv_Orderdetail = new System.Windows.Forms.DataGridView();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.Dgv_Orders = new System.Windows.Forms.DataGridView();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.Dgv_Product = new System.Windows.Forms.DataGridView();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dgv_Customers)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_Customers)).BeginInit();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_Orderdetail)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_Orders)).BeginInit();
+            this.tabPage4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_Product)).BeginInit();
             this.SuspendLayout();
             // 
-            // btn_Connect
+            // Btn_Connect
             // 
-            this.btn_Connect.Location = new System.Drawing.Point(12, 12);
-            this.btn_Connect.Name = "btn_Connect";
-            this.btn_Connect.Size = new System.Drawing.Size(90, 45);
-            this.btn_Connect.TabIndex = 0;
-            this.btn_Connect.Text = "資料庫開啟測試";
-            this.btn_Connect.UseVisualStyleBackColor = true;
-            this.btn_Connect.Click += new System.EventHandler(this.btn_Connect_Click);
+            this.Btn_Connect.Location = new System.Drawing.Point(12, 12);
+            this.Btn_Connect.Name = "Btn_Connect";
+            this.Btn_Connect.Size = new System.Drawing.Size(90, 45);
+            this.Btn_Connect.TabIndex = 0;
+            this.Btn_Connect.Text = "資料庫開啟測試";
+            this.Btn_Connect.UseVisualStyleBackColor = true;
+            this.Btn_Connect.Click += new System.EventHandler(this.Btn_Connect_Click);
             // 
-            // button1
+            // Btn_Read
             // 
-            this.button1.Location = new System.Drawing.Point(12, 75);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(90, 44);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "讀取單列測試";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.btn_Read_Click);
+            this.Btn_Read.Location = new System.Drawing.Point(12, 75);
+            this.Btn_Read.Name = "Btn_Read";
+            this.Btn_Read.Size = new System.Drawing.Size(90, 44);
+            this.Btn_Read.TabIndex = 2;
+            this.Btn_Read.Text = "讀取單列測試";
+            this.Btn_Read.UseVisualStyleBackColor = true;
+            this.Btn_Read.Click += new System.EventHandler(this.Btn_Read_Click);
             // 
             // tabControl1
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
             this.tabControl1.Location = new System.Drawing.Point(149, 12);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
@@ -72,7 +85,7 @@ namespace DataBaseTest
             // 
             // tabPage1
             // 
-            this.tabPage1.Controls.Add(this.dgv_Customers);
+            this.tabPage1.Controls.Add(this.Dgv_Customers);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
@@ -81,24 +94,72 @@ namespace DataBaseTest
             this.tabPage1.Text = "顧客";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
+            // Dgv_Customers
+            // 
+            this.Dgv_Customers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Dgv_Customers.Location = new System.Drawing.Point(3, 3);
+            this.Dgv_Customers.Name = "Dgv_Customers";
+            this.Dgv_Customers.RowTemplate.Height = 24;
+            this.Dgv_Customers.Size = new System.Drawing.Size(625, 394);
+            this.Dgv_Customers.TabIndex = 0;
+            // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.Dgv_Orderdetail);
             this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage2.Size = new System.Drawing.Size(631, 400);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.Text = "交易細項";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // dgv_Customers
+            // Dgv_Orderdetail
             // 
-            this.dgv_Customers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgv_Customers.Location = new System.Drawing.Point(19, 16);
-            this.dgv_Customers.Name = "dgv_Customers";
-            this.dgv_Customers.RowTemplate.Height = 24;
-            this.dgv_Customers.Size = new System.Drawing.Size(590, 366);
-            this.dgv_Customers.TabIndex = 0;
+            this.Dgv_Orderdetail.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Dgv_Orderdetail.Location = new System.Drawing.Point(3, 3);
+            this.Dgv_Orderdetail.Name = "Dgv_Orderdetail";
+            this.Dgv_Orderdetail.RowTemplate.Height = 24;
+            this.Dgv_Orderdetail.Size = new System.Drawing.Size(625, 394);
+            this.Dgv_Orderdetail.TabIndex = 0;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.Dgv_Orders);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(631, 400);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "交易列表";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // Dgv_Orders
+            // 
+            this.Dgv_Orders.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Dgv_Orders.Location = new System.Drawing.Point(3, 3);
+            this.Dgv_Orders.Name = "Dgv_Orders";
+            this.Dgv_Orders.RowTemplate.Height = 24;
+            this.Dgv_Orders.Size = new System.Drawing.Size(625, 394);
+            this.Dgv_Orders.TabIndex = 0;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.Dgv_Product);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(631, 400);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "產品";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // Dgv_Product
+            // 
+            this.Dgv_Product.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Dgv_Product.Location = new System.Drawing.Point(3, 3);
+            this.Dgv_Product.Name = "Dgv_Product";
+            this.Dgv_Product.RowTemplate.Height = 24;
+            this.Dgv_Product.Size = new System.Drawing.Size(625, 394);
+            this.Dgv_Product.TabIndex = 0;
             // 
             // Form1
             // 
@@ -106,25 +167,36 @@ namespace DataBaseTest
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.tabControl1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.btn_Connect);
+            this.Controls.Add(this.Btn_Read);
+            this.Controls.Add(this.Btn_Connect);
             this.Name = "Form1";
             this.Text = "Form1";
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dgv_Customers)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_Customers)).EndInit();
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_Orderdetail)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_Orders)).EndInit();
+            this.tabPage4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Dgv_Product)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button btn_Connect;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Btn_Connect;
+        private System.Windows.Forms.Button Btn_Read;
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.DataGridView dgv_Customers;
+        private System.Windows.Forms.DataGridView Dgv_Customers;
         private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.DataGridView Dgv_Orderdetail;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.DataGridView Dgv_Orders;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.DataGridView Dgv_Product;
     }
 }
 
--- DataBaseTest/DataBaseTest/DataBaseTest/Form1.cs
+++ DataBaseTest/DataBaseTest/DataBaseTest/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -13,24 +14,54 @@ namespace DataBaseTest
 {
     public partial class Form1 : Form
     {
-        //SqlConnection cnn;
-        SqlConnectionStringBuilder sConnB;
-        //SqlCommand command;
+        public SqlConnectionStringBuilder sConnB;
         //@"Data Source=AOI-142\\SQLEXPRESS;Initial Catalog=MVC_TestDB;User ID=AOI;Password = aoi0817;"(string)
-        string sDataSource = "AOI-142\\SQLEXPRESS";
-        string sInitialCatalog = "MVC_TestDB";
-        string sUserID = "AOI";
-        string sPassword = "aoi0817";
+        public string sDataSource = "AOI-142\\SQLEXPRESS";
+        public string sInitialCatalog = "MVC_TestDB";
+        public string sUserID = "AOI";
+        public string sPassword = "aoi0817";
 
-        string sTable_Customers = "Table_Customers";
-        string sTable_Orderdetial = "Table_Orderdetial";
-        string sTable_orders = "Table_orders";
-        string sTable_Products = "Table_Products";
+        public string sTable_Customers = "Table_Customers";
+        public string sTable_Orderdetail = "Table_Orderdetial";
+        public string sTable_Orders = "Table_orders";
+        public string sTable_Product = "Table_Products";
 
-        enum Customers
+        
+        public class CustomersDB
         {
+            public string Id { get; set; }
+            public string Name { get; set; }
+            public string Tel { get; set; }
+            public string Address { get; set; }
+            public string InitDate { get; set; }
         }
+        public class OrderdetailDB
+        {
+            public string Id { get; set; }
+            public string OrderID { get; set; }
+            public string ProductID { get; set; }
+            public string Amount { get; set; }
+            public string Price { get; set; }
+            public string Total { get; set; }
+            public string Memo { get; set; }
+            public string InitDate { get; set; }
+        }
+        public class OrdersDB
+        {
+            public string Id { get; set; }
+            public string CustomerID { get; set; }
+            public string Total { get; set; }
+            public string InitDate { get; set; }
 
+        }
+        public class ProductDB
+        {
+            public string Id { get; set; }
+            public string Name { get; set; }
+            public string Price { get; set; }
+            public string InitDate { get; set; }
+        }
+        //-----------------------
         public Form1()
         {
             InitializeComponent();
@@ -42,33 +73,133 @@ namespace DataBaseTest
                 UserID = sUserID,
                 Password = sPassword
             };
+            Stopwatch swStopwatch = new Stopwatch();
+            swStopwatch.Restart();
+            All_dgv_ReFlash();
+            swStopwatch.Stop();
+            TimeSpan trim = swStopwatch.Elapsed;
+            Console.WriteLine("迴圈1次時間: " + trim + "\n目前時間: " + DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss.fff tt"));
         }
-        private void btn_Connect_Click(object sender, EventArgs e)
+
+        private void All_dgv_ReFlash()
+        {
+            Dgv_Customers_ReFlash();
+            Dgv_Orderdetail_ReFlash();
+            Dgv_Orders_ReFlash();
+            Dgv_Product_ReFlash();
+        }
+        private void Dgv_Customers_ReFlash()
+        {
+            using (SqlConnection cnn = new SqlConnection(sConnB.ConnectionString))
+            {
+                cnn.Open();
+
+                string squery = $"SELECT * FROM {sTable_Customers}";
+                using (SqlCommand command = new SqlCommand(squery, cnn))
+                {
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        //DataTable dataTable = new DataTable();
+                        DataSet dataSet = new DataSet();
+                        adapter.Fill(dataSet, "Customers");
+                        Dgv_Customers.DataSource = dataSet;
+                        Dgv_Customers.DataMember = "Customers";
+                    }
+                }
+                cnn.Close();//可不用 using會藉由 IDisposable 清除
+                cnn.Dispose();
+            }
+        }
+        private void Dgv_Orderdetail_ReFlash()
+        {
+            using (SqlConnection cnn = new SqlConnection(sConnB.ConnectionString))
+            {
+                cnn.Open();
+
+                string squery = $"SELECT * FROM {sTable_Orderdetail}";
+                using (SqlCommand command = new SqlCommand(squery, cnn))
+                {
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        DataSet dataSet = new DataSet();
+                        adapter.Fill(dataSet, "Orderdetail");
+                        Dgv_Orderdetail.DataSource = dataSet;
+                        Dgv_Orderdetail.DataMember = "Orderdetail";
+                    }
+                }
+                cnn.Close();//可不用 using會藉由 IDisposable 清除
+                cnn.Dispose();
+            }
+        }
+        private void Dgv_Orders_ReFlash()
+        {
+            using (SqlConnection cnn = new SqlConnection(sConnB.ConnectionString))
+            {
+                cnn.Open();
+
+                string squery = $"SELECT * FROM {sTable_Orders}";
+                using (SqlCommand command = new SqlCommand(squery, cnn))
+                {
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        DataSet dataSet = new DataSet();
+                        adapter.Fill(dataSet, "Orders");
+                        Dgv_Orders.DataSource = dataSet;
+                        Dgv_Orders.DataMember = "Orders";
+                    }
+                }
+                cnn.Close();//可不用 using會藉由 IDisposable 清除
+                cnn.Dispose();
+            }
+        }
+        private void Dgv_Product_ReFlash()
+        {
+            using (SqlConnection cnn = new SqlConnection(sConnB.ConnectionString))
+            {
+                cnn.Open();
+
+                string squery = $"SELECT * FROM {sTable_Product}";
+                using (SqlCommand command = new SqlCommand(squery, cnn))
+                {
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        DataSet dataSet = new DataSet();
+                        adapter.Fill(dataSet, "Product");
+                        Dgv_Product.DataSource = dataSet;
+                        Dgv_Product.DataMember = "Product";
+                    }
+                }
+                cnn.Close();//可不用 using會藉由 IDisposable 清除
+                cnn.Dispose();
+            }
+        }
+
+
+        private void Btn_Connect_Click(object sender, EventArgs e)
         {
             try
             {
-                using(SqlConnection cnn = new SqlConnection(sConnB.ConnectionString))
+                using (SqlConnection cnn = new SqlConnection(sConnB.ConnectionString))
                 {
                     cnn.Open();
                     MessageBox.Show("資料庫開啟成功!");
                     cnn.Close();//可不用 using會藉由 IDisposable 清除
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 MessageBox.Show(ex.Message, "OPEN", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
-        private void btn_Read_Click(object sender, EventArgs e)
+        private void Btn_Read_Click(object sender, EventArgs e)
         {
             using (SqlConnection cnn = new SqlConnection(sConnB.ConnectionString))
             {
                 cnn.Open();
 
                 string squery = $"SELECT address FROM {sTable_Customers}";
-                using (SqlCommand command = new SqlCommand(squery,cnn))
+                using (SqlCommand command = new SqlCommand(squery, cnn))
                 {
-
                     using (SqlDataReader reader = command.ExecuteReader())
                     {
                         int address = reader.GetOrdinal("Address");
@@ -80,6 +211,7 @@ namespace DataBaseTest
                     }
                 }
                 cnn.Close();//可不用 using會藉由 IDisposable 清除
+                cnn.Dispose();
             }
         }
 
