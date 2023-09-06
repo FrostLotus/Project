a4337fcec88aeee188206bdc764ca9a3b301da40
frederick <wr264359@gmail.com>
2023-09-06 09:02:18 +0800

    以UA為基礎 以INI讀取為基礎 以MFC那份為基礎

--- OCPUAServer_Form/OCPUAServer_Form/OCPUAServer_Form/Form1.Designer.cs
+++ OCPUAServer_Form/OCPUAServer_Form/OCPUAServer_Form/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace OCPUAServer_Form
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// 設計工具所需的變數。
@@ -29,13 +29,308 @@ namespace OCPUAServer_Form
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Btn_Add = new System.Windows.Forms.Button();
+            this.Btn_Delete = new System.Windows.Forms.Button();
+            this.Btn_UpdateFile = new System.Windows.Forms.Button();
+            this.Btn_UpdateMember = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Btn_Stop = new System.Windows.Forms.Button();
+            this.Btn_Run = new System.Windows.Forms.Button();
+            this.Lab_Status = new System.Windows.Forms.Label();
+            this.button7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(93, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(321, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(93, 52);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(321, 22);
+            this.textBox2.TabIndex = 0;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(93, 84);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(321, 22);
+            this.textBox3.TabIndex = 0;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(93, 138);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(321, 22);
+            this.textBox4.TabIndex = 0;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(93, 166);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(321, 22);
+            this.textBox5.TabIndex = 0;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(93, 194);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(321, 22);
+            this.textBox6.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(93, 112);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(321, 20);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Var Num";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Item Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Node Id";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Type";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 141);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 12);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Length";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(26, 169);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(32, 12);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Initial";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(26, 197);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 12);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Value";
+            // 
+            // Btn_Add
+            // 
+            this.Btn_Add.Location = new System.Drawing.Point(435, 19);
+            this.Btn_Add.Name = "Btn_Add";
+            this.Btn_Add.Size = new System.Drawing.Size(99, 30);
+            this.Btn_Add.TabIndex = 3;
+            this.Btn_Add.Text = "Add";
+            this.Btn_Add.UseVisualStyleBackColor = true;
+            this.Btn_Add.Click += new System.EventHandler(this.Btn_Add_Click);
+            // 
+            // Btn_Delete
+            // 
+            this.Btn_Delete.Location = new System.Drawing.Point(435, 55);
+            this.Btn_Delete.Name = "Btn_Delete";
+            this.Btn_Delete.Size = new System.Drawing.Size(99, 30);
+            this.Btn_Delete.TabIndex = 3;
+            this.Btn_Delete.Text = "Delete";
+            this.Btn_Delete.UseVisualStyleBackColor = true;
+            this.Btn_Delete.Click += new System.EventHandler(this.Btn_Delete_Click);
+            // 
+            // Btn_UpdateFile
+            // 
+            this.Btn_UpdateFile.Location = new System.Drawing.Point(435, 91);
+            this.Btn_UpdateFile.Name = "Btn_UpdateFile";
+            this.Btn_UpdateFile.Size = new System.Drawing.Size(99, 30);
+            this.Btn_UpdateFile.TabIndex = 3;
+            this.Btn_UpdateFile.Text = "Update_File";
+            this.Btn_UpdateFile.UseVisualStyleBackColor = true;
+            this.Btn_UpdateFile.Click += new System.EventHandler(this.Btn_UpdateFile_Click);
+            // 
+            // Btn_UpdateMember
+            // 
+            this.Btn_UpdateMember.Location = new System.Drawing.Point(435, 127);
+            this.Btn_UpdateMember.Name = "Btn_UpdateMember";
+            this.Btn_UpdateMember.Size = new System.Drawing.Size(99, 30);
+            this.Btn_UpdateMember.TabIndex = 3;
+            this.Btn_UpdateMember.Text = "Update_Member";
+            this.Btn_UpdateMember.UseVisualStyleBackColor = true;
+            this.Btn_UpdateMember.Click += new System.EventHandler(this.Btn_UpdateMember_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 233);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(402, 317);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Btn_Stop);
+            this.groupBox1.Controls.Add(this.Btn_Run);
+            this.groupBox1.Controls.Add(this.Lab_Status);
+            this.groupBox1.Location = new System.Drawing.Point(420, 233);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(134, 161);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Server狀態";
+            // 
+            // Btn_Stop
+            // 
+            this.Btn_Stop.Location = new System.Drawing.Point(15, 71);
+            this.Btn_Stop.Name = "Btn_Stop";
+            this.Btn_Stop.Size = new System.Drawing.Size(99, 30);
+            this.Btn_Stop.TabIndex = 3;
+            this.Btn_Stop.Text = "STOP";
+            this.Btn_Stop.UseVisualStyleBackColor = true;
+            this.Btn_Stop.Click += new System.EventHandler(this.Btn_Stop_Click);
+            // 
+            // Btn_Run
+            // 
+            this.Btn_Run.Location = new System.Drawing.Point(15, 21);
+            this.Btn_Run.Name = "Btn_Run";
+            this.Btn_Run.Size = new System.Drawing.Size(99, 30);
+            this.Btn_Run.TabIndex = 3;
+            this.Btn_Run.Text = "RUN";
+            this.Btn_Run.UseVisualStyleBackColor = true;
+            this.Btn_Run.Click += new System.EventHandler(this.Btn_Run_Click);
+            // 
+            // Lab_Status
+            // 
+            this.Lab_Status.AutoSize = true;
+            this.Lab_Status.Location = new System.Drawing.Point(27, 128);
+            this.Lab_Status.Name = "Lab_Status";
+            this.Lab_Status.Size = new System.Drawing.Size(73, 12);
+            this.Lab_Status.TabIndex = 2;
+            this.Lab_Status.Text = "SERVER停止";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(435, 520);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(99, 30);
+            this.button7.TabIndex = 3;
+            this.button7.Text = "Close";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(560, 563);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Btn_UpdateMember);
+            this.Controls.Add(this.Btn_UpdateFile);
+            this.Controls.Add(this.Btn_Delete);
+            this.Controls.Add(this.Btn_Add);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "MainForm";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button Btn_Add;
+        private System.Windows.Forms.Button Btn_Delete;
+        private System.Windows.Forms.Button Btn_UpdateFile;
+        private System.Windows.Forms.Button Btn_UpdateMember;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button Btn_Stop;
+        private System.Windows.Forms.Button Btn_Run;
+        private System.Windows.Forms.Label Lab_Status;
+        private System.Windows.Forms.Button button7;
     }
 }
 
--- OCPUAServer_Form/OCPUAServer_Form/OCPUAServer_Form/Form1.cs
+++ OCPUAServer_Form/OCPUAServer_Form/OCPUAServer_Form/Form1.cs
@@ -6,15 +6,256 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Opc.Ua;
+using Opc.Ua.Server;
 using System.Windows.Forms;
+using System.Threading;
+using System.IO;
 
 namespace OCPUAServer_Form
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+
+        private MyOpcServer opcServer;
+
+        public MainForm()
         {
             InitializeComponent();
+
+        }
+        public void ReadTXTFile()
+        {
+
+        }
+        public void SaveTXTFile()
+        {
+
+        }
+
+
+        
+        //-------------------------------------------------------
+        private void Btn_Run_Click(object sender, EventArgs e)
+        {
+            // 启动OPC服务器
+            opcServer = new MyOpcServer();
+            //opcServer.Start()
+        }
+
+        private void Btn_Stop_Click(object sender, EventArgs e)
+        {
+            // 停止OPC服务器
+            opcServer.Stop();
+        }
+        //----------
+        private void Btn_Add_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Btn_Delete_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Btn_UpdateFile_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Btn_UpdateMember_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+
+    public static class Param
+    {
+        public static string DefaultPath = Application.StartupPath + @"\Datatext";
+        public static string DataFilePath = DefaultPath + "\\OPCServer_Value.ini";
+        public static void LoadData()
+        {
+            #region 沒有就創建
+            if (!File.Exists(DataFilePath))
+            {
+                DataFilePath = DefaultPath + "\\OPCServer_Value.ini";
+                using (StreamWriter createfile = File.CreateText(DataFilePath))
+                {
+                    MessageBox.Show("無指定資料,已生成");
+                    createfile.Close();
+                }
+            }
+            #endregion
+
+        }
+        public static void Break_String(string sData, string sBreakItem, ref List<object> list)
+        {
+            list.Clear();
+            string[] Spare = sData.Split(new string[] { sBreakItem }, StringSplitOptions.None);
+            foreach (string item in Spare)//餵回去list
+            {
+                list.Add(item);
+            }
+        }
+    }
+    //==================================================
+
+    public class OpcDataItem
+    {
+        public int VarNumber { get; set; }//變數序
+        public string ItemName { get; set; }//對應中文名稱(client不顯示)
+        public string NodeID { get; set; }//ID名稱
+        public BuiltInType Type { get; set; }//數值類型:word(uint)
+        public int DataLength { get; set; }//數值長度(string:?)
+        public object Initial { get; set; }//預設值
+        public object Value { get; set; }//數值
+
+        public OpcDataItem(int varNumber, string itemName, string nodeID, BuiltInType type, int dataLength, object initial)
+        {
+            VarNumber = varNumber;
+            ItemName = itemName;
+            NodeID = nodeID;
+            Type = type;
+            DataLength = dataLength;
+            Initial = initial;
+            //Value = value;
+        }
+
+        public BaseDataVariableState CreateNode()
+        {
+            var node = new BaseDataVariableState(null)
+            {
+                NodeId = new NodeId(NodeID),
+                BrowseName = new QualifiedName(ItemName),
+                DisplayName = new LocalizedText(ItemName),
+                DataType = NodeID,//???
+                ValueRank = ValueRanks.Scalar,
+                Value = new Variant(Value),
+                AccessLevel = AccessLevels.CurrentReadOrWrite
+            };
+            return node;
+        }
+    }
+
+    public class MyOpcServer : StandardServer
+    {
+
+        private List<OpcDataItem> opcDataItems = new List<OpcDataItem>();
+        public MyOpcServer()
+        {
+            InitializeDataItems();
+        }
+        /// <summary>
+        /// 資料添加
+        /// </summary>
+        private void InitializeDataItems()
+        {
+            //(第一筆)
+            //變數序\n
+            //對應中文名稱\n
+            //ID名稱\n
+            //數值類型:word(uint)\n
+            //數值長度(string:?)\n
+            //預設值\n
+            //[空行]
+
+            // 添加你的OPC数据项到opcDataItems列表中
+            // 每个数据项包含编号、名称、ID、数据类型、数据长度和初始值
+            // 例如：
+            //                              Var/  Name(中)/ NodeID(實際)/      類型/        長度/ 數值/
+            //opcDataItems.Add(new OpcDataItem(1, "Item1", "ns=2;s=Item1", BuiltInType.Float, 4, 0.0f));
+            //opcDataItems.Add(new OpcDataItem(2, "Item2", "ns=2;s=Item2", BuiltInType.Int32, 4, 0));
+            Param.LoadData();
+
+
+            #region 判斷後讀取
+            
+            List<object> DataList = new List<object>();
+            List<object> SPList = new List<object>();
+                 
+            var lines = File.ReadAllLines(Param.DataFilePath);
+            OpcDataItem currentDataItem = null;
+
+            try
+            {
+                foreach (var line in lines)
+                {
+                    // 如果是空行，表示為分隔符
+                    if (string.IsNullOrWhiteSpace(line))
+                    {
+                        currentDataItem = null;
+                        continue;
+                    }
+                    // 按照 Key=Value 格式解析每一行
+                    var parts = line.Split('=');
+                    if (parts.Length == 2)
+                    {
+                        var key = parts[0].Trim();
+                        var value = parts[1].Trim();
+
+                        // 根据键（Key）來更新屬性
+                        if (currentDataItem != null)
+                        {
+                            switch (key)
+                            {
+                                case "VarNumber":
+                                    currentDataItem.VarNumber = int.Parse(value);
+                                    break;
+                                case "ItemName":
+                                    currentDataItem.ItemName = value;
+                                    break;
+                                case "NodeID":
+                                    currentDataItem.NodeID = value;
+                                    break;
+                                case "Type":
+                                    currentDataItem.Type = (BuiltInType)Enum.Parse(typeof(BuiltInType), value);
+                                    break;
+                                case "DataLength":
+                                    currentDataItem.DataLength = int.Parse(value);
+                                    break;
+                                case "Initial":
+                                    if (currentDataItem.Type == BuiltInType.Float)
+                                    {
+                                        currentDataItem.Value = float.Parse(value);
+                                    }
+                                    else if (currentDataItem.Type == BuiltInType.String)
+                                    {
+                                        currentDataItem.Value = value;
+                                    }
+                                    else if (currentDataItem.Type == BuiltInType.Boolean)
+                                    {
+                                        currentDataItem.Value=(value="1") = ;
+                                    }
+                                    break;
+                            }
+                        }
+                    }
+
+                }
+             
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "載入資料", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            #endregion
+            // 添加更多数据项
+        }
+        protected override void OnServerStarting(ApplicationConfiguration configuration)
+        {
+            base.OnServerStarting(configuration);
+
+            // 在此处配置服务器设置
+
+            // 启动OPC数据项
+            foreach (var item in opcDataItems)
+            {
+                //AddPredefinedNode(null, item.CreateNode());
+            }
+        }
+        protected override void Dispose(bool disposing)
+        {
+            // 清理资源
+            base.Dispose(disposing);
         }
     }
 }
